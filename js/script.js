9a94a7d80a9bce4aedb09452deb74d2b9e522d23
FelipeVanceto07 <139002208+FelipeVanceto07@users.noreply.github.com>
2023-10-28 17:28:14 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
-// tres constantes para referenciar as ITEM nas Divs Projetos
-const formulario = document.getElementById('formulario');
-const site = docuemnt.getElementById('Site');
-const prot = document.getElementById('Meuportifolio');
+/* tres constantes para referenciar as ITEM nas Divs Projetos
+as Item nas divs Projetos
+*/
 
-const videoformulario = document.querySelector('.videoformulario');
-const videoSite = document.querySelector('.videoSite');
-const videoMeuportifolio = document.querySelector('.videoMeuportifolio');
+// 3 constantes para referenciar os Id das Divs no Projetos
+const album = document.getElementById('album');
+const casamentos= document.getElementById('casamentos');
+const  buffet= document.getElementById('Buffet');
+
+
+// 3 constantes para referenciar os Id das Divs no Projetos
+const videoAlbum = document.querySelector('#videoAlbum');
+const videocasamentos = document.querySelector('#videocasamaentos');
+const videoBuffet = document.querySelector('#videoBuffet');
 
 // evento de click
-formulario.addEventListener('click',() =>{
-   formulario.classList.toggle('active');
-   videoformulario.classList.toggle('active');
+album.addEventListener('click',() =>{
+   album.classList.toggle('active'); //para iframe */
+   videoAlbum.classList.toggle('active');
+}); 
+casamentos.addEventListener('click',() =>{
+   casamentos.classList.toggle('active'); //para iframe */
+   videoCasamentos.classList.toggle('active');
+});
+buffet.addEventListener('click',() =>{
+buffet.classList.toggle('active'); //para iframe */
+   videoBuffet.classList.toggle('active');
 });
